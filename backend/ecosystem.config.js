3d7da2b02675c0aa570180cd30037d0781e99e42
Lokrand <Uchiha132132qqq@gmail.com>
2023-07-05 21:55:25 +0300

    changes 3.0

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -26,7 +26,7 @@ module.exports = {
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
-      'post-deploy': `cd ~/backend/source/backend/ && npm i && npm run build && pm2 restart ecosystem.config.js && pm2 save`,
+      'post-deploy': `cd ~/backend/source/backend/ && npm run build && pm2 restart ecosystem.config.js && pm2 save`,
     },
   },
 };
