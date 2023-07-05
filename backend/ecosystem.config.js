ce82baaa7ed445c49c365c20404c32dd6bba2d43
Dmitriy Platonov <dplatonov@adway.team>
2023-07-05 11:22:28 +0300

    add auto deploy pm2 configs

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPO, DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'backend',
+    script: './dist/app.js',
+  }],
+
+  // Настройка деплоя
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/backend`,
+      'post-deploy': `cd ~/${DEPLOY_PATH}/backend && npm i && npm run build && pm2 restart ecosystem.config.js && pm2 save`,
+    },
+  },
+};
---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPO, DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'frontend',
+    script: './dist/app.js',
+  }],
+
+  // Настройка деплоя
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'post-deploy': `cd ~/${DEPLOY_PATH}/frontend && npm i && npm run build`,
+    },
+  },
+};
