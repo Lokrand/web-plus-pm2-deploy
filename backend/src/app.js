ab433d998da1064ae61b4e2a741cc625e878a7ad
Lokrand <Uchiha132132qqq@gmail.com>
2023-07-06 10:53:47 +0300

    changes 10.0

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ var express_1 = require("express");
 var mongoose_1 = require("mongoose");
 var cookie_parser_1 = require("cookie-parser");
 var celebrate_1 = require("celebrate");
-// import cors from 'cors';
+import cors from 'cors';
 var error_handler_1 = require("./middlewares/error-handler");
 var config_1 = require("./config");
 var routes_1 = require("./routes");
@@ -13,7 +13,7 @@ var _a = process.env.PORT, PORT = _a === void 0 ? 3000 : _a;
 var app = (0, express_1.default)();
 mongoose_1.default.connect(config_1.DB_ADDRESS);
 // Только для локальных тестов. Не используйте это в продакшене
-// app.use(cors())
+app.use(cors())
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, cookie_parser_1.default)());
